d3b8b7b4da677aceb4fa942f5cbac80572158572
Roee <roibenezra0@gmail.com>
2023-05-22 08:54:05 +0300

    resolve tester comments

--- include/PriorityQueue.h
+++ include/PriorityQueue.h
@@ -17,32 +17,28 @@ class PriorityQueue {
 
 public:
     // push a new value into the queue
-    void push(const T &t);
+    void push(const T &t) {
+        // find the position to insert the new value based on its priority
+        auto it = m_data.cbegin();
+        auto comp = MyComparator<T>()(*it, t);
+        while (it != m_data.cend() && comp < 0) {
+            ++it;
+        }
+        // insert the new value at the found position
+        m_data.insert(it, t);
+    }
+
 
     // pop the value with the highest priority
-    T poll();
+    T poll() {
+        if (m_data.empty()) {
+            throw std::out_of_range("PriorityQueue is empty");
+        }
+        T item = m_data.front();
+        m_data.pop_front();
+        return item;
+    }
 
 private:
     std::list <T> m_data;
 };
-
-template<typename T>
-void PriorityQueue<T>::push(const T &t) {
-    // find the position to insert the new value based on its priority
-    auto it = m_data.cbegin();
-    while (it != m_data.cend() && MyComparator<T>()(*it, t) < 0) {
-        ++it;
-    }
-    // insert the new value at the found position
-    m_data.insert(it, t);
-}
-
-template<typename T>
-T PriorityQueue<T>::poll() {
-    if (m_data.empty()) {
-        throw std::out_of_range("PriorityQueue is empty");
-    }
-    T item = m_data.front();
-    m_data.pop_front();
-    return item;
-}
